516073032e1819753bfeaf6e77bd55bef0d72f33
Franco <francotorrico1999@gmail.com>
2025-03-27 21:12:36 -0300

    punto 4 final con gitignore y corregido

---
+++ index.js
@@ -0,0 +1,15 @@
+function cambiarNombre() {
+    let nombre = "Carlos";
+    nombre = "Pedro"; 
+
+    console.log("Nuevo nombre:", nombre);
+
+    //mostrar(nombre);
+    let saludo = "Bienvenido "
+    let resultado = saludo + nombre; 
+    console.log(resultado)
+}
+
+cambiarNombre();
+// 'Nuevo nombre: Pedro'
+// 'Bienvenido Pedro'
